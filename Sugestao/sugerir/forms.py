05fd900842fa074039b06e9561d76013d0cc3da3
Ricardo Sousa Pimentel <2306214@ifto.local>
2020-02-20 17:05:57 -0300

    Altaração na página de sugestões

--- Sugestao/sugerir/forms.py
+++ Sugestao/sugerir/forms.py
@@ -1,13 +1,33 @@
 from django import forms
+from django.utils.datetime_safe import datetime
 
+import Sugestao
 from Sugestao.core.models import sugestao
 
 
-class SugestaoForm(forms.ModelForm):
+class SugestaoForm(forms.Form):
+    setor = forms.ChoiceField(label="Setor")
+    pessoa = forms.ChoiceField(label="Pessoa")
     descricao = forms.CharField(label="Sugestão", widget=forms.Textarea())
     imagem = forms.ImageField(label="", required=False, widget=forms.FileInput(attrs={'accept': 'image/*'}))
-    datahora = forms.DateTimeField(label='')
 
-    class Meta:  # Define os campos vindos do Model
-        model = sugestao
-        fields = ('setor', 'pessoa', 'descricao', 'imagem', 'datahora')
+
+    def __init__(self, request, SETORES, PESSOAS, *args, **kwargs):
+        super(SugestaoForm, self).__init__(*args, **kwargs)
+        self.request = request
+        self.fields['setor'].choices = SETORES
+        self.fields['pessoa'].choices = PESSOAS
+
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        setor = cleaned_data.get("setor")
+        pessoa = cleaned_data.get("pessoa")
+        descricao = cleaned_data.get("descricao")
+        imagem = cleaned_data.get("imagem")
+
+        sugestaoobj = sugestao(setor=Sugestao.core.models.setor.objects.get(id=setor), pessoa=Sugestao.core.models.pessoa.objects.get(id=pessoa), descricao=descricao, imagem=imagem, datahora=datetime.now())
+        sugestaoobj.save()
+        print(sugestaoobj.id)
+
+        return cleaned_data
--- Sugestao/sugerir/views.py
+++ Sugestao/sugerir/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect, resolve_url as r, render_to_response
-from Sugestao.core.models import sugestao
+
+import Sugestao
+from Sugestao.core.models import setor, pessoa, sugestao
 from Sugestao.sugerir.forms import SugestaoForm
 
 
@@ -11,11 +13,27 @@ def FazerSugestao(request):
     except KeyError:
         return redirect(r('Login'))
 
-    form = SugestaoForm()
+
+    #Preencher Formulário
+    SETORES = []
+    setorobj = setor.objects.all()
+    for set in setorobj:
+        SETORES.append((set.id, set.nome))
+    PESSOAS = []
+    pessoaobj = pessoa.objects.filter(nome="Anônimo") | pessoa.objects.filter(nome=request.session['nome'])
+    for pess in pessoaobj:
+        PESSOAS.append((pess.id, pess.nome))
+    form = SugestaoForm(request, SETORES, PESSOAS)
+
     if request.method == 'POST':
-        form = SugestaoForm(request.POST, request.FILES)
+        form = SugestaoForm(request, SETORES, PESSOAS, request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            #sugestaoobj = sugestao(setor=Sugestao.core.models.setor.objects.get(id=setor), pessoa=Sugestao.core.models.pessoa.objects.get(id=pessoa), descricao=descricao, imagem=imagem, datahora=datetime.now())
+            #sugestaoobj.save()
+            #print(sugestaoobj.id)
             messages.success(request, 'Configurações salvas com sucesso!')
+            return redirect(r('FazerSugestao'))
+        else:
+            messages.success(request, 'Erro ao salvar sua sujestão')
             return redirect(r('FazerSugestao'))
     return render(request, 'sugerir/cadastro_sugestao.html', {'err': '','form': form, 'itemselec': 'HOME'})
