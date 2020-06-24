efcf190dd66c565bf2d58a285f6ca80325a7cf27
Ricardo Sousa Pimentel <2306214@ifto.local>
2020-06-24 11:13:34 -0300

    correção na imagem default das sugestões

--- Sugestao/settings.py
+++ Sugestao/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/sugestao/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/sugestao/media/'
-MEDIA_ROOT = '/storage'
+MEDIA_ROOT = '/app/storage'
 
 # Email configuration
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
